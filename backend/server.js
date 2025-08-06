40efe6ebc5ca5e0519cb68f22c6375a82b208fc1
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-08-07 02:52:39 +0700

    setup socket.io server and create connection listener

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const http = require('http')
-const server = http.createServer(app)
+const server = http.createServer(app) // we have to use http server directly to integrate with socket.io
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 const port = 5000
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
 })
 
 server.listen(port, () => {
