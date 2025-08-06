407cd23c43b84b05e8d898574421676ca86fdc84
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-08-06 19:23:18 +0700

    Make simple chat ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
+const handleSubmit = (e) => {
+  e.preventDefault()
+}
+
 function App() {
-  return <>Yo</>
+  return (
+    <main>
+      <h1 className="text-3xl m-3">Socket.io Chat</h1>
+      <section className="m-3 ">
+        <ul className="flex flex-col bg-purple-100 p-2  rounded-xl min-h-96">
+          <li className="bg-blue-300 text-white rounded-lg p-2 m-1 w-fit text-overflow">
+            Hello!
+          </li>
+          <li className="bg-gray-400 rounded-lg p-2 m-1 w-fit self-end text-overflow">
+            Hi there!
+          </li>
+        </ul>
+
+        <form action="send-message" className="flex items-baseline">
+          <input
+            className="mt-2 rounded border border-gray-300 px-1 py-0.5 w-full focus:outline-none"
+            type="text"
+            placeholder="type messages..."
+          />
+          <button
+            className="bg-green-500 text-white rounded py-1 px-3 mx-1"
+            onClick={handleSubmit}
+          >
+            Send
+          </button>
+        </form>
+      </section>
+    </main>
+  )
 }
 
 export default App
