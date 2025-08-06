992f50a86f9181913f897a305a6b0f54948c66ff
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-08-07 03:03:15 +0700

    allow all origins for developement

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app) // we have to use http server directly to integrate with socket.io
 const { Server } = require('socket.io')
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
 
 const port = 5000
 
@@ -12,7 +16,7 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  console.log('a user connected', socket.id)
 })
 
 server.listen(port, () => {
